8706dec39e3991e768b4d4fba2d261d8cfb54084
Hamza mimoz <mimizou-hamza@hotmail.com>
2024-09-09 19:15:01 +0200

    i create the 2 function fir human and the compture

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
-console.log("Hello World")
+let humanScore = 0 ;
+let computerScore = 0 ; 
+
+
+
+function getComputerChoice(){
+    let choice_numbre = Math.floor( Math.random()*3)
+    if (choice_numbre === 0 ){
+    let choice_comptre = " rock"
+    console.log( choice_comptre)
+    }
+    else if (choice_numbre === 1 ){
+    choice_comptre = " paper"
+    console.log( choice_comptre)
+    }
+    else {choice_comptre = "scissors"
+    console.log( choice_comptre) };
+}
+
+function getHumanChoice(){ 
+
+    let choice_humane = prompt(" entre your choice rock, papre or scissors : ")
+
+    return console.log( choice_humane)
+}
+
+
+
+getHumanChoice()
+
+getComputerChoice()
